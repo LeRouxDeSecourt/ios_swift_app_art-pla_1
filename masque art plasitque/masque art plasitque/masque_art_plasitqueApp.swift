8c4e3676e5c095ea9e2ec247d52afeebd7a201b1
LeRouxDeSecourt <167676894+LeRouxDeSecourt@users.noreply.github.com>
2026-02-16 11:36:22 +0100

    Commit de base avec la création du projet.

---
+++ masque art plasitque/masque art plasitque/masque_art_plasitqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  masque_art_plasitqueApp.swift
+//  masque art plasitque
+//
+//  Created by Alexandre Capon on 16/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct masque_art_plasitqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
